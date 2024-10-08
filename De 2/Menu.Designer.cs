8941521b126689cbf62ed86d32e814cf6d157666
MilkTeaChuoi <milktavichuoi@gmail.com>
2024-10-08 14:17:34 +0700

    Main form

--- De 2/Menu.Designer.cs
+++ De 2/Menu.Designer.cs
@@ -40,7 +40,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(145, 53);
+            this.label1.Location = new System.Drawing.Point(158, 53);
             this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(212, 25);
